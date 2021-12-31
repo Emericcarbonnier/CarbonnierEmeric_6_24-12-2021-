c9ac6ef5ec99b760948abde31677f19939f24f58
Emericcarbonnier <pro.emeric@laposte.net>
2021-12-31 12:44:17 +0100

    install mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
+mongoose.connect('mongodb+srv://eme1394:13111994@piquanteopenclassroom.rcawi.mongodb.net/PiquanteOpenClassRoom?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+
+module.exports = app;
