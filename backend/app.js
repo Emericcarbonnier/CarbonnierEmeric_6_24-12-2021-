3663dfbe0bf787a39fa05605918946aed5d88993
Emericcarbonnier <pro.emeric@laposte.net>
2022-01-03 10:51:16 +0100

    connection & login OK

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ mongoose.connect('mongodb+srv://eme1394:13111994@piquanteopenclassroom.rcawi.mon
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+  app.use(express.json());
 
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
